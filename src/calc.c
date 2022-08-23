a2ac624ce3cf02bf9342e86365f31f9c8b2f8b39
Serzhunya <kon-serzhunya@yandex.ru>
2022-08-23 10:38:57 +0300

    dgf

--- src/calc.c
+++ src/calc.c
@@ -1,30 +1,29 @@
-#include "s21_calc.h"
+// #include "s21_calc.h"
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-int create_lexem(char* input, char* array_lexem[300]) {
+int create_buffer(char* input, char* buffer[]) {
   int res = 0;
   char* sign = "+-*/()^m";
-  char* s1 = input;
-  int index;
-  // нашли первый индекс вхождения
-  for (int i = 0; i < strlen(s1); i++) {
-    index = strcspn(s1, sign);  // получили индекс первого делиметра
-    strncpy((unsigned char*)array_lexem, s1, index);  // в 0 индексе лежит 2222
-    while (*s1 != '\0' && *s1 != *array_lexem) {
-      s1++;
+  for (int i = 0; i < strlen(input); i++) {
+    if(*input >= 48 && *input <= 57 && *input == 46) {
+      buffer[i] = input;
     }
   }
-
-  //   printf("%d", index);
   return res;
 }
 
 int main(void) {
-  char* input = "2222+3+2.2";
-  char* array_lexem[255];
 
-  create_lexem(input, array_lexem);
-  //   for (int i = 0; i < 1; i++) {
-  printf("%s", &array_lexem[0]);
-  //   }
+  char* input = "2222+3+2.2";
+  char* buffer[255];
+
+  create_buffer(input, buffer);
+
+  // for (int i = 0; i < strlen(*buffer); i++) {
+  printf("%s", buffer[0]);
+  // }
   return 0;
 }
