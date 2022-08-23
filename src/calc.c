08d0b41ada1a101aab145f15ba77f5583152de3b
Sandslash Dorla <sandslas@oa-i4.msk.21-school.ru>
2022-08-23 19:50:54 +0300

    new

--- src/calc.c
+++ src/calc.c
@@ -1,29 +1,46 @@
-// #include "s21_calc.h"
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-int create_buffer(char* input, char* buffer[]) {
+int create_buffer_digit(char* input, char* buffer) {
   int res = 0;
-  char* sign = "+-*/()^m";
-  for (int i = 0; i < strlen(input); i++) {
-    if(*input >= 48 && *input <= 57 && *input == 46) {
-      buffer[i] = input;
+  int count = 0;
+  int count_sign = 0;
+  char* stak = calloc(1, sizeof(200));
+  for (int i = 0; i <= strlen(input); i++) {
+    if ((input[i] >= 48 && input[i] <= 57) || input[i] == 46) {
+      buffer[count++] = input[i];
+    } else {
+      buffer[count++] = ' ';
+    }
+  }
+  for (int i = 0; i <= strlen(input); i++) {
+    if ((input[i] >= 40 && input[i] <= 43) || input[i] == 45 ||
+        input[i] == 45) {
+      stak[count_sign++] = input[i];
     }
   }
   return res;
 }
 
+// int create_buffer_sign(char* input, char* buffer) {
+//   int res = 0;
+//   int count = 0;
+//   for (int i = 0; i <= strlen(input); i++) {
+//     if ((input[i] >= 48 && input[i] <= 57) || input[i] == 46) {
+//       buffer[count++] = input[i];
+//     } else {
+//       buffer[count++] = ' ';
+//     }
+//   }
+//   return res;
+// }
+
 int main(void) {
-
-  char* input = "2222+3+2.2";
-  char* buffer[255];
-
-  create_buffer(input, buffer);
-
-  // for (int i = 0; i < strlen(*buffer); i++) {
-  printf("%s", buffer[0]);
-  // }
+  char* input = "2222+3+(2.2+35)";
+  char* buffer_digit = calloc(1, sizeof(255));
+  create_buffer_digit(input, buffer_digit);
+  printf("buffer:%s", buffer_digit);
   return 0;
 }
