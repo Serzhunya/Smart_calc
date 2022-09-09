4cbd06c6902f4a19f249b2eb7f3cee5f006d244f
Sandslash Dorla <sandslas@ga-p6.msk.21-school.ru>
2022-09-09 19:44:04 +0300

    qt

--- src/smart_calc/mainwindow.cpp
+++ src/smart_calc/mainwindow.cpp
@@ -61,24 +61,36 @@ MainWindow::~MainWindow() { delete ui; }
 void MainWindow::digits_numbers() {
   QPushButton *button = (QPushButton *)(sender());
   QString new_label;
-  if (!(ui->label->text() == "0")) {
+  QString new_label_2;
+  if (ui->label->text() != "0") {
     new_label = ui->label->text() + button->text();
   } else {
     new_label = button->text();
   }
   ui->label->setText(new_label);
+  if (ui->label_2->text() != "0") {
+    new_label_2 = ui->label_2->text() + button->text();
+  } else {
+    new_label_2 = button->text();
+  }
+  ui->label_2->setText(new_label_2);
 }
 
 void MainWindow::on_pushButton_dot_clicked() {
   if (!ui->label->text().contains('.')) {
     ui->label->setText(ui->label->text() + ".");
   }
+  if (!ui->label_2->text().contains('.')) {
+    ui->label_2->setText(ui->label_2->text() + ".");
+  }
 }
 
 void MainWindow::operations() {
   QPushButton *button = (QPushButton *)sender();
   double numbers;
+  double numbers_2;
   QString new_label;
+  QString new_label_2;
   if (button->text() == "+/-") {
     numbers = (ui->label->text()).toDouble();
     numbers = numbers * -1;
@@ -87,8 +99,19 @@ void MainWindow::operations() {
   } else if (button->text() == "%") {
     numbers = (ui->label->text()).toDouble();
     numbers = numbers * 0.01;
-    new_label = QString::number(numbers, 'g', 15);  // преобразование в строку
+    new_label = QString::number(numbers, 'g', 15);
     ui->label->setText(new_label);
+  }
+  if (button->text() == "+/-") {
+    numbers_2 = (ui->label_2->text()).toDouble();
+    numbers_2 = numbers_2 * -1;
+    new_label_2 = QString::number(numbers_2, 'g', 15);
+    ui->label_2->setText(new_label_2);
+  } else if (button->text() == "%") {
+    numbers_2 = (ui->label_2->text()).toDouble();
+    numbers_2 = numbers_2 * 0.01;
+    new_label_2 = QString::number(numbers_2, 'g', 15);
+    ui->label_2->setText(new_label_2);
   }
 }
 
@@ -96,7 +119,16 @@ void MainWindow::math_operations() {
   QPushButton *button = (QPushButton *)
       sender();  //  возвращает указатель на объект, пославший сигнал
   button->setChecked(true);
-  ui->label->setText(ui->label->text() + button->text());
+  if (ui->label->text() != "0") {
+    ui->label->setText(ui->label->text() + button->text());
+  } else {
+    ui->label->setText(button->text());
+  }
+  if (ui->label_2->text() != "0") {
+    ui->label_2->setText(ui->label_2->text() + button->text());
+  } else {
+    ui->label_2->setText(button->text());
+  }
 }
 
 void MainWindow::on_pushButton_clear_clicked() {
@@ -109,31 +141,109 @@ void MainWindow::on_pushButton_clear_clicked() {
 }
 
 void MainWindow::on_pushButton_result_clicked() {
-  QString text = ui->label->text();
-  ui->label_2->setText(text);
-  QByteArray str_bit =
-      text.toLocal8Bit();  // Преобразование QString в StringQByteArray
+  QString text = ui->label_2->text();
+  // ui->label_2->setText(text);
+  QByteArray str_bit = text.toLocal8Bit();
   char *input_str = str_bit.data();
-     int code = validation(input_str);
-     if (code) {
-        ui->label->setText("Error");
-     } else {
-  char *polish_notation = create_polish_notation(input_str, sign_st);
-  double output = create_output(polish_notation, sign_st, digit_st);
-  QString str_output = QString::number(output);
-  ui->label->setText(str_output);
-     }
+
+  int code = validation(input_str);
+  if (code) {
+    ui->label->setText("Error");
+    ui->label_2->setText("Error");
+  } else {
+    char *polish_notation = create_polish_notation(input_str, sign_st);
+    double output = create_output(polish_notation, sign_st, digit_st);
+    QString str_output = QString::number(output);
+    ui->label->setText(str_output);
+    ui->label_2->setText(str_output);
+  }
 }
 
 void MainWindow::braces_buttons() {
   QPushButton *button = (QPushButton *)sender();
   button->setChecked(true);
-  ui->label->setText(ui->label->text() + button->text());
+  if (ui->label->text() != "0") {
+    ui->label->setText(ui->label->text() + button->text());
+  } else {
+    ui->label->setText(button->text());
+  }
+  if (ui->label_2->text() != "0") {
+    ui->label_2->setText(ui->label_2->text() + button->text());
+  } else {
+    ui->label_2->setText(button->text());
+  }
 }
 
 void MainWindow::trigeometry_operations() {
-  QPushButton *button = (QPushButton *)
-      sender();  //  возвращает указатель на объект, пославший сигнал
+  QPushButton *button = (QPushButton *)sender();
   button->setChecked(true);
-  ui->label->setText(ui->label->text() + button->text() + '(');
+  if (button->text() == "sin") {
+    if (ui->label_2->text() != "0") {
+      ui->label_2->setText(ui->label_2->text() + "s(");
+    } else {
+      ui->label_2->setText("s(");
+    }
+  }
+  if (button->text() == "cos") {
+    if (ui->label_2->text() != "0") {
+      ui->label_2->setText(ui->label_2->text() + "c(");
+    } else {
+      ui->label_2->setText("c(");
+    }
+  }
+  if (button->text() == "tan") {
+    if (ui->label_2->text() != "0") {
+      ui->label_2->setText(ui->label_2->text() + "t(");
+    } else {
+      ui->label_2->setText("t(");
+    }
+  }
+  if (button->text() == "acos") {
+    if (ui->label_2->text() != "0") {
+      ui->label_2->setText(ui->label_2->text() + "a(");
+    } else {
+      ui->label_2->setText("a(");
+    }
+  }
+  if (button->text() == "asin") {
+    if (ui->label_2->text() != "0") {
+      ui->label_2->setText(ui->label_2->text() + "i(");
+    } else {
+      ui->label_2->setText("i(");
+    }
+  }
+  if (button->text() == "atan") {
+    if (ui->label_2->text() != "0") {
+      ui->label_2->setText(ui->label_2->text() + "n(");
+    } else {
+      ui->label_2->setText("n(");
+    }
+  }
+  if (button->text() == "sqrt") {
+    if (ui->label_2->text() != "0") {
+      ui->label_2->setText(ui->label_2->text() + "q(");
+    } else {
+      ui->label_2->setText("q(");
+    }
+  }
+  if (button->text() == "ln") {
+    if (ui->label_2->text() != "0") {
+      ui->label_2->setText(ui->label_2->text() + "l(");
+    } else {
+      ui->label_2->setText("l(");
+    }
+  }
+  if (button->text() == "log") {
+    if (ui->label_2->text() != "0") {
+      ui->label_2->setText(ui->label_2->text() + "o(");
+    } else {
+      ui->label_2->setText("o(");
+    }
+  }
+
+  if (ui->label->text() != "0") {
+    ui->label->setText(ui->label->text() + button->text() + '(');
+  } else {
+    ui->label->setText(button->text() + "(");
+  }
 }
--- src/smart_calc/validation.c
+++ src/smart_calc/validation.c
@@ -1,26 +1,75 @@
 #include "s21_calc.h"
 
-int validation(char* input_str) {
+int validation(char *input_str) {
   int code = 0;
+  int count_opened_braces = 0;
+  int count_closed_braces = 0;
+  if (input_str[0] == '*' || input_str[0] == '/' || input_str[0] == '^' ||
+      input_str[0] == '.' || input_str[0] == ')') {
+    code = 1;
+  }
+  if (input_str[strlen(input_str) - 1] == '*' ||
+      input_str[strlen(input_str) - 1] == '/' ||
+      input_str[strlen(input_str) - 1] == '^' ||
+      input_str[strlen(input_str) - 1] == '.' ||
+      input_str[strlen(input_str) - 1] == '(' ||
+      input_str[strlen(input_str) - 1] == '+' ||
+      input_str[strlen(input_str) - 1] == '-') {
+    code = 1;
+  }
   for (int i = strlen(input_str); i >= 0; i--) {
     if (strlen(input_str) == 0) {
       break;
     }
-    int index_1 = strcspn(input_str, "+-*/^");
-    char char_valid_1 = input_str[index_1 + 1];
-    if (char_valid_1 == '-' || char_valid_1 == '+' || char_valid_1 == '*' ||
-        char_valid_1 == '/' || char_valid_1 == '^') {
+    if (strchr(input_str, 40)) {
+      count_opened_braces++;
+    }
+    if (strchr(input_str, 41)) {
+      count_closed_braces++;
+    }
+    if (count_opened_braces != count_closed_braces) {
       code = 1;
     }
-    // char* lexem = create_lexem(input);
-    // input = input + strlen(lexem);
+    int index_1 = strcspn(input_str, "+-*/^");
+    int index_2 = strcspn(input_str, "0123456789");
+    int index_3 = strcspn(input_str, ".");
+    int index_4 = strcspn(input_str, ")");
+    int index_5 = strcspn(input_str, "(");
+    char char_valid_1 = input_str[index_1 + 1];
+    char char_valid_2 = input_str[index_2 + 1];
+    char char_valid_3 = input_str[index_3 + 1];
+    char char_valid_4 = input_str[index_4 + 1];
+    char char_valid_5 = input_str[index_5 + 1];
+    if (char_valid_1 == '-' || char_valid_1 == '+' || char_valid_1 == '*' ||
+        char_valid_1 == '/' || char_valid_1 == '^' || char_valid_1 == '.' ||
+        char_valid_1 == ')') {
+      code = 1;
+    }
+    if (char_valid_2 == 'c' || char_valid_2 == 's' || char_valid_2 == 't' ||
+        char_valid_2 == 'a' || char_valid_2 == '(' || char_valid_2 == 'l') {
+      code = 1;
+    }
+    if (char_valid_3 == 'c' || char_valid_3 == 's' || char_valid_3 == 't' ||
+        char_valid_3 == 'a' || char_valid_3 == '(' || char_valid_3 == ')' ||
+        char_valid_3 == '+' || char_valid_3 == '-' || char_valid_3 == 'l' ||
+        char_valid_3 == '*' || char_valid_3 == '/' || char_valid_3 == '^') {
+      code = 1;
+    }
+    if (char_valid_4 == 'c' || char_valid_4 == 's' || char_valid_4 == 't' ||
+        char_valid_4 == 'a' || char_valid_4 == 'l' || char_valid_4 == '(' ||
+        char_valid_4 == '1' || char_valid_4 == '2' || char_valid_4 == '3' ||
+        char_valid_4 == '4' || char_valid_4 == '5' || char_valid_4 == '6' ||
+        char_valid_4 == '7' || char_valid_4 == '8' || char_valid_4 == '9' ||
+        char_valid_4 == '0' || char_valid_4 == '.') {
+      code = 1;
+    }
+    if (char_valid_5 == ')' || char_valid_5 == '.' || char_valid_5 == '+' ||
+        char_valid_5 == '-' || char_valid_5 == '*' || char_valid_5 == '/' ||
+        char_valid_5 == '^') {
+      code = 1;
+    }
+    // input_str = input_str + index_1 + 1;
+    input_str = input_str++;
   }
-  //   int index_1 = strcspn(input_str, "+-*/^");
-  //   char char_valid_1 = input_str[index_1 + 1];
-  //   if (char_valid_1 == '-' || char_valid_1 == '+' || char_valid_1 == '*' ||
-  //       char_valid_1 == '/' || char_valid_1 == '^') {
-  //     code = 1;
-  //   }
-  //   input = input + strlen(lexem);
   return code;
 }
